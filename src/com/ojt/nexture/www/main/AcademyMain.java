63bee19c314cb76b53e8b0e279dfe0bf68c7deef
Unknown <ckdrnjs513@gmail.com>
2018-11-16 17:03:34 +0900

    TestEnd

--- src/com/ojt/nexture/www/main/AcademyMain.java
+++ src/com/ojt/nexture/www/main/AcademyMain.java
@@ -4,13 +4,7 @@ public class AcademyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("==========アカデミー管理システム==========");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("\t1．会員登録　　　2．ログイン");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("====================================");
+		
 	}
 
 }
