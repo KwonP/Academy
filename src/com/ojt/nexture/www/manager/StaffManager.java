052640f967d93166ac16114e536e73a9f23f0573
Kim Jae Ha <swi00417@naver.com>
2018-11-21 17:54:18 +0900

    VO, StaffManagerInterface

--- src/com/ojt/nexture/www/manager/StaffManager.java
+++ src/com/ojt/nexture/www/manager/StaffManager.java
@@ -4,5 +4,19 @@
 package com.ojt.nexture.www.manager;
 
 public interface StaffManager {
-
+	
+	public boolean fixStaff();
+	//정보수정
+	
+	public boolean joinStaff();
+	//가입
+	
+	public boolean deleteStaff();
+	//탈퇴
+	
+	public void logIn();
+	//로그인
+	
+	public void addClass();
+	//강의등록
 }
