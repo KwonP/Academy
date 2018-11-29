d1db9051589995bda0d6fcc84fa2c4a81bb81731
YoungJae Won <zz1004dog@naver.com>
2018-11-29 15:49:11 +0900

    UI,Manager, ManagerImpl, StaffManager,LectureVO
    2018-11-29

--- src/com/ojt/nexture/www/managerImpl/ProfessorManagerImpl.java
+++ src/com/ojt/nexture/www/managerImpl/ProfessorManagerImpl.java
@@ -8,7 +8,6 @@ import com.ojt.nexture.www.manager.ProfessorManager;
 
 public class ProfessorManagerImpl implements ProfessorManager {
 	Scanner sc = new Scanner(System.in);
-	int checkPhoneNum;
 	int checkNo;
 	int fixCheck, listNum;
 	int checkNum = 0;
@@ -74,41 +73,16 @@ public class ProfessorManagerImpl implements ProfessorManager {
 		return false;
 	}
 
-	@Override
-	public boolean joinProfessor(List<HumanVO> userList, HumanVO human) {
-		checkNo = 0;
-		if (userList.size() == 0) {
-			userList.add(human);
-		} else {
-			for (int i = 0; i < userList.size(); i++) {
-				if (human.getPhoneNum().equals(userList.get(i).getPhoneNum())) {
-					checkNo++;
-				}
-			}
-			if (checkNo > 0) {
-				checkPhoneNum = 1;
-				System.out.println("同じ電話番号があります。");
-			} else {
-				checkPhoneNum = 0;
-				userList.add(human);
-			}
-		}
-		for (int a = 0; a < userList.size(); a++) {
-			System.out.println(userList.get(a));
-		}
-
-		return true;
-	}
 
 	@Override
-	public void loginProfessor(List<HumanVO> userList, String userUinqNum, String userPassword) {
+	public void loginProfessor(List<HumanVO> userList, String userUinqNum) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < userList.size(); i++) {
 			if (userList.get(i).getUniqNum().equals(userUinqNum)) {
 				String pre = userList.get(i).getName();
 				System.out.println("---------------------------------");
 				System.out.println("ようこそ!" + pre + "さん!");
-				System.out.println("1.担当講義閲覧     2.講義入力     3.情報修正     4.退会     5.ログアウト");
+				System.out.println("1.担当講義閲覧     2.講義入力     3.情報修正     4.ログアウト");
 				System.out.println("---------------------------------");
 			}
 		}
@@ -116,41 +90,38 @@ public class ProfessorManagerImpl implements ProfessorManager {
 	}
 
 	@Override
-	public boolean fixProfessor(List<HumanVO> userList, HumanVO human, String userUinqNum) {
+	public boolean fixProfessor(List<LectureVO> lecList, LectureVO lecture, List<HumanVO> userList, String userUinqNum) {
 		// TODO Auto-generated method stub
-		for (int a = 0; a < userList.size(); a++) {
-			if (human.getPhoneNum().equals(userList.get(a).getPhoneNum())) {
-				fixCheck = 0;
+		if(sc.equals("y")||sc.equals("Y")) {
+			for (int a = 0; a < lecList.size(); a++) {
+				if (lecList.get(a).getLectureName().equals(lecture.getLectureName())) {
+					fixCheck = 0;
 
-			} else {
-				for (int i = 0; i < userList.size(); i++) {
-					if (userUinqNum.equals(userList.get(i).getPhoneNum())) {
-						fixCheck = 1;
-						listNum = i;
+				} else {
+					for (int i = 0; i < userList.size(); i++) {
+						if (userUinqNum.equals(lecList.get(i).getUniqNum())) {
+							fixCheck = 1;
+							listNum = i;
 
+						}
 					}
 				}
 			}
-		}
-		if (fixCheck == 0) {
-			System.out.println("同じ電話番号があります。");
-			System.out.println("修正を失敗しました。");
-			checkNum = 1;
-		}
-		if (fixCheck == 1) {
-			userList.set(listNum, human);
-			System.out.println("修正が完了しました。");
-			System.out.println("またログインしてください。");
-			checkNum = 2;
+			if (fixCheck == 0) {
+				System.out.println("同じ名前があります。");
+				System.out.println("修正を失敗しました。");
+				checkNum = 1;
+			}
+			if (fixCheck == 1) {
+				lecList.set(listNum, lecture);
+				System.out.println("修正が完了しました。");
+				System.out.println("またログインしてください。");
+				checkNum = 2;
+			}	
 		}
 		return true;
 	}
 
-
-	public int getCheckPhoneNum() {
-		// TODO Auto-generated method stub
-		return checkPhoneNum;
-	}
 
 
 	public int getCheckNum() {
